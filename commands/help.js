454275015eb29d97352192a5d1ed9affcf128907
Paul Sohns <paul.sohns@web.de>
2021-10-02 14:31:29 +0200

    Fix indents & update README

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,17 @@
 module.exports = {
-  name: 'help',
-  description: "this is a help command",
-  execute(message, config, commands) {
-    console.log('User ' + message.author.username + ' // ' + message.author.id + ' used the help command.');
+	name: 'help',
+  	description: "this is a help command",
+  	execute(message, config, commands) {
+    	console.log('User ' + message.author.username + ' // ' + message.author.id + ' used the help command.');
 
-    let help_msg = "👋 Hayo, the commands which I can help you with are:\n";
+    	let help_msg = "👋 Hayo, the commands which I can help you with are:\n";
 
-    for(const [_, command] of commands) {
-      help_msg += `\n\`${config.prefix}${command.name}\`: ${command.description}`;
-    }
+    	for(const [_, command] of commands) {
+      		help_msg += `\n\`${config.prefix}${command.name}\`: ${command.description}`;
+    	}
 
-    help_msg += `\n\nI am still missing some data regarding the schedules, so If you want me to add any, just message "Sokker - Paulo#0001" here on discord! 👀`;
+    	help_msg += `\n\nI am still missing some data regarding the schedules, so If you want me to add any, just message "Sokker - Paulo#0001" here on discord! 👀`;
 
-    message.channel.send(help_msg);
-  }
+    	message.channel.send(help_msg);
+  	}
 }
